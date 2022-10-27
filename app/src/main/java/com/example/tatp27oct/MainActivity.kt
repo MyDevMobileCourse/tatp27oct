60a1f75b6d63f800aa735b2aa480ac926dee40b2
youssefchlendi <youssef.chlendi@gmail.com>
2022-10-27 21:16:36 +0100

    add state images

--- app/src/main/java/com/example/tatp27oct/MainActivity.kt
+++ app/src/main/java/com/example/tatp27oct/MainActivity.kt
@@ -4,6 +4,33 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 
 class MainActivity : AppCompatActivity() {
+    var listOfTunisianStatesCombinedWithPicture = listOf(
+        Pair("Ariana", R.drawable.ariana),
+        Pair("Beja", R.drawable.beja),
+        Pair("Ben Arous", R.drawable.benarous),
+        Pair("Bizerte", R.drawable.bizerte),
+        Pair("Gabès", R.drawable.gabes),
+        Pair("Gafsa", R.drawable.gafsa),
+        Pair("Jendouba", R.drawable.jendouba),
+        Pair("Kairouan", R.drawable.kairouan),
+        Pair("Kasserine", R.drawable.kasserine),
+        Pair("Kébili", R.drawable.kebili),
+        Pair("Kef", R.drawable.kef),
+        Pair("Mahdia", R.drawable.mahdia),
+        Pair("Manouba", R.drawable.manouba),
+        Pair("Médenine", R.drawable.medenine),
+        Pair("Monastir", R.drawable.monastir),
+        Pair("Nabeul", R.drawable.nabeul),
+        Pair("Sfax", R.drawable.sfax),
+        Pair("Sidi Bouzid", R.drawable.sidibouzid),
+        Pair("Siliana", R.drawable.siliana),
+        Pair("Sousse", R.drawable.sousse),
+        Pair("Tataouine", R.drawable.tataouine),
+        Pair("Tozeur", R.drawable.tozeur),
+        Pair("Tunis", R.drawable.tunis),
+        Pair("Zaghouan", R.drawable.zaghouan)
+    )
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
